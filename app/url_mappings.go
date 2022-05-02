5785a2e0902d50bb0206fc243b38f31d57cf15b4
idoyudhatama <ido.yudhatama@mbiz.co.id>
2022-05-03 06:47:34 +0700

    move controllers package

--- app/url_mappings.go
+++ app/url_mappings.go
@@ -1,10 +1,13 @@
 package app
 
-import "bookstore_users-api/controllers"
+import (
+	"bookstore_users-api/controllers/ping"
+	"bookstore_users-api/controllers/users"
+)
 
 func mapUrls() {
-	router.GET("/ping", controllers.Ping)
+	router.GET("/ping", ping.Ping)
 
-	router.GET("/users/:user_id", controllers.GetUser)
-	router.POST("/users", controllers.CreateUser)
+	router.GET("/users/:user_id", users.GetUser)
+	router.POST("/users", users.CreateUser)
 }
